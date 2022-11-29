f6b4ef98b62d862b036dbf6a9c3b1a43a9dbe69b
Uyen Doan <catuyen99@gmail.com>
2022-11-29 13:51:27 -0600

    Uyen - Changed header bar

--- movie_system/src/app/movie-details/movie-details.component.ts
+++ movie_system/src/app/movie-details/movie-details.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
-
+  reviews: any = [
+    {
+      "username": "amy94",
+      "review": "Great movie! Thought I would not cry at a family, comedy movie but yes, crying like a baby."
+    },
+    {
+      "username": "smmr.rrr",
+      "review": "Would really recommmend! Such a wonderful movie."
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {

f5c8996defced3931a08086859883253aaf66766
Thu Nguyen <66782390+ThuNguyen-198@users.noreply.github.com>
2022-12-02 17:08:24 -0600

    Adjust UI when user login

--- movie_system/src/app/header-bar/header-bar.component.ts
+++ movie_system/src/app/header-bar/header-bar.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AccountService } from '../auth/account.service';
 
 @Component({
   selector: 'app-header-bar',
   templateUrl: './header-bar.component.html',
   styleUrls: ['./header-bar.component.css']
 })
-export class HeaderBarComponent implements OnInit {
-  userLoggedIn = true;
+export class HeaderBarComponent implements OnInit, OnDestroy {
   username = 'smmr.rrr';
-  constructor() { }
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription = Subscription.EMPTY;
 
-  ngOnInit(): void {
+  constructor(public accountService: AccountService) {
   }
 
+  ngOnInit(): void {
+    this.authListenerSubs = this.accountService.getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
+  }
 }
